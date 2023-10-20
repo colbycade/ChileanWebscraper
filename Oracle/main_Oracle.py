5cce30aa2ce30e20fc6f0a52b0842d66328119a0
colbycade <colbycade@gmail.com>
2023-10-20 16:17:16 -0600

    add progress bar for db updates

--- Oracle/main_Oracle.py
+++ Oracle/main_Oracle.py
@@ -3,11 +3,12 @@
 
 import requests
 import time
+from tqdm import tqdm
 from ScrapeChile.webscraper import scrape_page
 from ScrapeChile.utility import print_def_data
 from update_Oracle_db import update_Oracle
 
-ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+ALPHABET = 'xabcdefghijklmnopqrstuvwxyz'
 # ALPHABET = 'x'
 
 if __name__ == '__main__':
@@ -21,8 +22,7 @@ if __name__ == '__main__':
         while retries < max_retries:
             try:
                 entries = scrape_page(letter)
-                print('Updating database...')
-                for entry_name, definitions in entries.items():
+                for entry_name, definitions in tqdm(entries.items(), desc="Updating database...", unit="updates", leave=False):
                     for definition_data in definitions:
                         # print_def_data(entry_name, definition_data) # uncomment to print
                         update_Oracle(entry_name, definition_data)
@@ -32,11 +32,11 @@ if __name__ == '__main__':
             except requests.RequestException as e:  # Catching network errors
                 print(f"An error occurred: {e}")
                 retries += 1
-                print(f"Retrying {letter} in {retry_delay} seconds...")
+                print(f"Retrying in {retry_delay} seconds...")
                 time.sleep(retry_delay)
             except Exception as e:  # Catching other exceptions
                 print(f"An unknown error occurred: {e}")
                 retries += 1
-                print(f"Retrying {letter} in {retry_delay} seconds...")
+                print(f"Retrying in {retry_delay} seconds...")
                 time.sleep(retry_delay)
 
